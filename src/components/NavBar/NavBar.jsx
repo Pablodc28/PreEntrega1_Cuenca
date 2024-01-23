32a6c704edc1a5def88f99aea2ba3c2aa36d0bf6
Pablodc28 <Pablodc28@gmail.com>
2024-01-22 22:55:26 -0300

    PreEntrega2

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,28 +1,38 @@
 import CartWidget from "../CartWidget/CartWidget"
-
+import { Link , NavLink  } from "react-router-dom"
 
 const NavBar = () => {
   return (
     <div>
-           <nav className="navbar navbar-expand-lg  bg-info border-bottom border-body">
+           <nav className="navbar navbar-expand-lg  bg-info border-bottom border-body fixed-top ">
            <div className="container-fluid">
            
-                      <a className="navbar-brand" href="#">Tienda Mariolo</a>
-           
+                      <Link to="/" className="navbar-brand">
+                          Tienda El Ramblón
+                      </Link>
+
                       <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                       <span className="navbar-toggler-icon"></span>
                       </button>
 
            <div className="collapse navbar-collapse " id="navbarNav">
                       <ul className="navbar-nav ms-auto ">
+
                                  <li className="nav-item">
-                                            <a className="nav-link active" aria-current="page" href="#">Zapatillas</a>
+                                          <NavLink to="/categoria/1" className="nav-link">
+                                              Pantalones
+                                          </NavLink>
+                                 </li>
+
+                                 <li className="nav-item">
+                                        <NavLink to="/categoria/2" className="nav-link">
+                                            Remeras y Camisas
+                                         </NavLink>
                                  </li>
                                  <li className="nav-item">
-                                            <a className="nav-link" href="#">Pantalones</a>
-                                 </li>
-                                 <li className="nav-item">
-                                            <a className="nav-link" href="#">Gorros</a>
+                                      <NavLink to="/categoria/3" className="nav-link">
+                                            Seguridad Ind
+                                        </NavLink>
                                  </li>                      
                       </ul>
            </div>
